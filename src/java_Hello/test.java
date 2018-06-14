9701cc1bc257045f26b6c32cf3ec6ab59266279d
BiliBiliCK <530910789@qq.com>
2018-06-14 16:20:08 +0800

    新建

---
+++ src/java_Hello/test.java
@@ -0,0 +1,11 @@
+package java_Hello;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int re=9;
+			System.out.println(re+"34");
+	}
+
+}
