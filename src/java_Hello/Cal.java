2dd851be1397efe418084645e5be51cc9689dc4a
BiliBiliCK <530910789@qq.com>
2018-06-15 18:03:25 +0800

    新增java

---
+++ src/java_Hello/Cal.java
@@ -0,0 +1,8 @@
+package java_Hello;
+
+public class Cal {
+
+	public int calCount(int num1, int num2) {
+		return  num1+num2;
+	}
+}
--- src/java_Hello/test.java
+++ src/java_Hello/test.java
@@ -1,12 +1,25 @@
 package java_Hello;
 
+/**
+ * @author CK
+ *
+ */
 public class test {
 
+	
+	/**
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			String re="≤‚ ‘";
-			System.out.println(re+"34");
+			String re="123";
+			String re2="4";
 			
+			int num1=Integer.parseInt(re);
+			int num2=Integer.parseInt(re2);
+			Cal cal=new Cal();
+			System.out.println(cal.calCount(num1,num2));
+		
 	}
 
 }
