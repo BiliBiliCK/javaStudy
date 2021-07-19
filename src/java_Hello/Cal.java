8184a1ece0cb2aea80e018b116ee1cd90962010c
CK <530910789@qq.com>
2021-07-19 23:44:09 +0800

    format code

--- src/java_Hello/Cal.java
+++ src/java_Hello/Cal.java
@@ -2,7 +2,7 @@ package java_Hello;
 
 public class Cal {
 
-	public int calCount(int num1, int num2) {
-		return  num1+num2;
-	}
+    public int calCount(int num1, int num2) {
+        return num1 + num2;
+    }
 }
--- src/java_Hello/hi.java
+++ src/java_Hello/hi.java
@@ -2,12 +2,34 @@ package java_Hello;
 
 import core.Mana;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.LinkedList;
+
 class hi {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Mana mana=new Mana();
-		mana.open();
-	}
+    public static int length = 3048576; //10的20次幂
+    private static ArrayList al = new ArrayList(length);
+    private static LinkedList ll = new LinkedList();
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        addNode(1);
+        addNode(0);
+    }
+
+    private static void addNode(Integer sign) {
+        Long start = new Date().getTime();
+        for (int i = 0; i < length; i++) {
+
+            if (sign == 1) {
+                al.add(i);
+            } else {
+                ll.add(i);
+            }
+        }
+        Long end = new Date().getTime();
+        System.out.println(sign + ":" + (end - start));
+    }
 
 }
